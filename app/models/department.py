ba0cbf328d5552f61b632a382aa3f53c828053f5
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-10-10 02:29:31 +0700

    create department and employee models

--- app/models/department.py
+++ app/models/department.py
@@ -1,1 +1,5 @@
 from django.db import models
+
+class Department(models.Model):
+  code = models.CharField(max_length=10)
+  name = models.CharField(max_length=100)
--- app/models/employee.py
+++ app/models/employee.py
@@ -1,1 +1,10 @@
 from django.db import models
+from app.models import Department
+
+class Employee(models.Model):
+  department = models.ForeignKey(Department, on_delete=models.CASCADE)
+  name_title = models.CharField(max_length=6)
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=100)
+  hire_date = models.DateField(auto_now_add=True)
+  employee_type = models.CharField(max_length=10)
