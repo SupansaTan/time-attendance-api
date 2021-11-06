be90daa9c6270e9dbc5bd3f1ef680dc1c565e7f9
hathaipat-p <hathaipat44@hotmail.co.th>
2021-11-06 16:44:35 +0700

    add column in department, employee

--- app/models/department.py
+++ app/models/department.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 class Department(models.Model):
-  code = models.CharField(max_length=10)
-  name = models.CharField(max_length=100)
+  parent_code = models.CharField(max_length=10,default='None')
+  dep_code = models.CharField(max_length=10, default='None')
+  name = models.CharField(max_length=100, default='None')
+  active_employee = models.IntegerField(default=0)
+  total_employee = models.IntegerField(default=0)
+  manager_id = models.IntegerField(default=0)
--- app/models/employee.py
+++ app/models/employee.py
@@ -1,10 +1,13 @@
 from django.db import models
-from app.models import Department
 
 class Employee(models.Model):
   employee_id = models.AutoField(primary_key=True)
-  name_title = models.CharField(max_length=6)
-  first_name = models.CharField(max_length=50)
-  last_name = models.CharField(max_length=100)
+  name_title = models.CharField(max_length=6,default='None')
+  first_name = models.CharField(max_length=50,default='None')
+  last_name = models.CharField(max_length=50,default='None')
+  employee_department = models.CharField(max_length=10)
   hire_date = models.DateField(auto_now_add=True)
-  employee_type = models.CharField(max_length=10)
+  employee_type = models.CharField(max_length=10,default='None')
+  role = models.CharField(max_length=10,default='None')
+  email = models.CharField(max_length=50,default='None')
+  password = models.CharField(max_length=50,default='None')
