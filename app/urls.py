20e17f35b1411acee71f9d65c61088052006e4c8
hathaipat-p <hathaipat44@hotmail.co.th>
2021-11-11 03:15:05 +0700

    add url

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
+from app.views import department, plan_shift, shift_code, time_record
 from . import views
 
 app_name = 'time_attendance'
 
 urlpatterns = [
-  path('api/employees', views.employee_list)
+  path('api/employees', views.employee_list),
+  path('api/departments', department.department_list),
+  path('api/planshift', plan_shift.plan_list),
+  path('api/shiftcode', shift_code.shift_code),
+  path('api/timerecord', time_record.time_record),
 ]
