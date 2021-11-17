6bc8850fb0d923449b1d8e32c7e979bdec21d1d2
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-11-17 09:58:06 +0700

    using django-heroku

--- time_attendance_api/settings.py
+++ time_attendance_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +133,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
