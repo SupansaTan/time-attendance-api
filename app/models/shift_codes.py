1e8f7414241979bfde0133fe1e81d70287c62cf3
Supansa <s6201012620236@email.kmutnb.ac.th>
2021-11-25 09:19:48 +0700

    change shift code max length

--- app/models/shift_codes.py
+++ app/models/shift_codes.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class ShiftCode(models.Model):
-    code = models.CharField(max_length=10)
+    code = models.CharField(max_length=15)
     start_time = models.TimeField(auto_now=False, auto_now_add=False)
     end_time = models.TimeField(auto_now=False, auto_now_add=False)
     start_break = models.TimeField(auto_now=False, auto_now_add=False)
